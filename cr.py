165b37be9332de817de1035b36b95fbef25fe0cd
BnanaOnTheWall <niushineshine@gmail.com>
2013-06-07 23:27:18 +0800

    cr.py is ready. Next integrate flickr api

--- cr.py
+++ cr.py
@@ -12,9 +12,6 @@ directory_list = ['http://unmatchedstyle.com/gallery/']
 for i in range(2, 11):
     previous_directory_url = "http://unmatchedstyle.com/gallery/page/" + str(i)
     directory_list.append(previous_directory_url)
-#     print "%s added to directory list" % previous_directory_url
-
-# print"directorly list successfully loaded."
 
 def inner_page_url(directory_url):
     raw_html = urlopen(directory_url).read()
@@ -22,37 +19,50 @@ def inner_page_url(directory_url):
     url_pattern = re.compile(r'''<a class="permalink" rel="bookmark" href="(http://unmatchedstyle.com/gallery/.+\.php)"''')
     inner_url_list = url_pattern.findall(raw_html)
 
-#     for i in inner_url_list:
-#         print i
+    return inner_url_list
 
-# inner_page_url("http://unmatchedstyle.com/gallery")  
+def get_info(url):
+    url = url.strip()
+    # url: http://unmatchedstyle.com/gallery/magician-slider.php
+    out_folder = re.findall(r'com/gallery/(.+)\.php$', url)[0].strip()
+    if not os.path.exists(out_folder):
+        os.makedirs(out_folder)
 
-# def imgdown(url):
+    raw_html = urlopen(url).read()
 
-#     out_folder = re.findall(r'\d+-(.+)', url)[0].strip()
+    # <h2 class="post-title">Magician Slider</h2> and <img src="http://unmatchedstyle.com/wp-content/uploads/2013/05/magicianslider.jpg" alt="" />
 
-#     if not os.path.exists(out_folder):
-#         os.makedirs(out_folder)
+    extraction_pattern = re.compile(r'''<h2 class="post-title">(.+)</h2>.*?<li class="visit"><a href="(.+)">Visit Site</a></li>.*?<img src="(http://unmatchedstyle.com/wp-content/uploads/\d{4}/\d{2}/.+?\.(jpg|png))" alt="" />''', re.DOTALL)
+    extracted_info = extraction_pattern.search(raw_html)
 
-#     raw = urlopen(url).read()
+    try:
+        site_title = extracted_info.group(1)
+    except:
+        site_title = "Error"
 
-#     all_img_url = re.findall(r'''src="/(.+?)" style''', raw)
-#     for img_url in all_img_url:
-#         real_img_url = "http://models.sight-management.com/" + img_url.strip()
-#         img_name = img_url.strip()[20:-11]
-#         outpath = os.path.join(out_folder, img_name)
-#         try:
-#             urlretrieve(real_img_url, outpath)
-#         except:
-#             continue
+    try:
+        site_url = extracted_info.group(2)
+    except:
+        site_url = url
 
-# raw = urlopen(url).read()
-# all_model_url = re.findall(r"href='/models/(.+?)'", raw)
+    try:
+        img_url = extracted_info.group(3)
+    except:
+        img_url = "http://unmatchedstyle.com/wp-content/uploads/2013/04/Converge-smallgraphic.jpg"
 
-# for model_url in all_model_url:
-#     real_model_url = "http://models.sight-management.com/models/" + model_url.strip()
-#     print "downloading images from %s" % real_model_url
-#     try:
-#         imgdown(real_model_url)
-#     except:
-#         continue
+    txt_filename = "info.txt"
+    with open(os.path.join(out_folder, txt_filename), 'w') as txt_file:
+        txt_file.write(site_title+"\n"+site_url+"\n"+img_url)
+        txt_file.close()
+
+    img_name = img_url.strip()[53:]
+    outpath = os.path.join(out_folder, img_name)
+    urlretrieve(img_url, outpath)
+
+counter = 0
+for i in directory_list:
+    for j in inner_page_url(i):
+        counter += 1
+        print "No.%s downloading %s" % (counter, j)
+        get_info(j)
+
